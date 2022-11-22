f163bfa464c3718787b96d2c6f636f799df437b1
Uday Gohel <114012274+UdayGohel@users.noreply.github.com>
2022-11-22 19:41:41 +0530

    Add files via upload

---
+++ PRIZEPOOL.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t,a,b;
+	cin>>t;
+	for(int i=1;i<=t;i++)
+	{
+	    cin>>a>>b;
+	    cout<<(a*10)+(b*90)<<endl;
+	}
+	return 0;
+}
