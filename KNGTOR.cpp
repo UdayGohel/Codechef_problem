b56bddae566adf40b3d66def1586f4d07fa40493
Uday Gohel <114012274+UdayGohel@users.noreply.github.com>
2022-12-02 19:42:56 +0530

    Add files via upload

---
+++ KNGTOR.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t,a,b;
+	cin>>t;
+	for(int i=1;i<=t;i++)
+	{
+	    cin>>a>>b;
+	    cout<<(a*5)+(b*7)<<endl;
+	}
+	return 0;
+}
