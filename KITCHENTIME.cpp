7f19973c61b4030fafe7d254fc2f7c5ada9288b4
Uday Gohel <114012274+UdayGohel@users.noreply.github.com>
2022-12-22 22:05:07 +0530

    Add files via upload

---
+++ KITCHENTIME.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int n,a,b;
+	cin>>n;
+	for(int i=1;i<=n;i++)
+	{
+	    cin>>a>>b;
+	    cout<<b-a<<endl;
+	}
+	return 0;
+}
